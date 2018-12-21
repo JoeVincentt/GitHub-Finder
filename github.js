154270f40016920094bedd14423fce6da927ee2d
JoeVincentt <eugene.bb@hotmail.com>
2018-12-20 22:52:35 -0500

    init commit

---
+++ github.js
@@ -0,0 +1,23 @@
+class Github {
+    constructor() {
+        //comes from gitHub website
+        this.client_id = '51ae707e33eefab53ea6';
+        this.client_secret = '7f80c4f7e96b9487ce7de82288638450d563dcc5';
+        this.repos_count = 5;
+        this.repos_sort = 'created: asc';
+    }
+
+    async getUser(user) {
+        const profileResponse = await fetch(`https://api.github.com/users/${user}?client_id=${this.client_id}&client_secret=${this.client_secret}`);
+
+        const repoResponse = await fetch(`https://api.github.com/users/${user}/repos?per_page=${this.repos_count}&sort=${this.repos_sort}client_id=${this.client_id}&client_secret=${this.client_secret}`);
+
+        const profile = await profileResponse.json();
+        const repos = await repoResponse.json();
+
+        return {
+            profile,
+            repos
+        }
+    }
+}
